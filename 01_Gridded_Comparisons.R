1b40961c9bc99dbb4de1b86a925a1867da968523
malloryb <mallorybarnes@email.arizona.edu>
2018-09-26 13:37:02 -0400

    Cleaned up a little. Trying to make plots. May rearrange and do one site at a time.

--- 01_Gridded_Comparisons.R
+++ 01_Gridded_Comparisons.R
@@ -44,16 +44,15 @@ Format_Ameriflux <- function(x){
   x$TS <- (x$LW_OUT_1_1_1/(sigma *(x$emiss)))^(0.25)
   return(x)}
 
-Mms_all <- Format_Ameriflux(US_Mms)
+x <- Format_Ameriflux(US_Mms)
 
 #Get plots of average winter vs. summer diurnal signals
 winter <- subset(x, year=="2010" & month=="01")
 win <- ddply(winter, .(time), summarize, TA = mean(TA, na.rm=TRUE),TS= mean(TS, na.rm=TRUE))
 summer <- subset(x, year=="2010" & month=="07")
 sum <- ddply(summer, .(time), summarize, TA = mean(TA, na.rm=TRUE),TS= mean(TS, na.rm=TRUE))
-print(head(sum))
-splot <- ggplot(sum, aes(x=time, group=1))+geom_line(aes(y=TA), colour="blue", size=1) + geom_line(aes(y=TS-273.15), colour="red", size=1)+labs(title="Daily summertime temp", y="Temperature (c)",x="Date") +theme_minimal()
-wplot <- ggplot(win, aes(x=time, group=1))+geom_line(aes(y=TA), colour="blue", size=1) + geom_line(aes(y=TS-273.15), colour="red", size=1)+labs(title="Daily wintertime temp", y="Temperature (c)",x="Date") +theme_minimal()
+splot <- ggplot(sum, aes(x=time, group=1))+geom_line(aes(y=TA), colour="blue", size=1) + geom_line(aes(y=TS-273.15), colour="red", size=1)+labs(title="Daily summertime temp", y="Temperature (c)",x="Time of Day") +theme_minimal()
+wplot <- ggplot(win, aes(x=time, group=1))+geom_line(aes(y=TA), colour="blue", size=1) + geom_line(aes(y=TS-273.15), colour="red", size=1)+labs(title="Daily wintertime temp", y="Temperature (c)",x="Time of Day") +theme_minimal()
 (grid.arrange(wplot, splot))
 
 #Get daily data
@@ -69,11 +68,33 @@ temp <- temp[Reduce(`&`, lapply(temp, is.finite)),]
 
 #Plot 1: 
 ggplot(temp, aes(x=date)) + 
-  geom_point(aes(y=Tower_TAavg), colour="blue", size=1.5) +
+  geom_point(aes(y=Tower_TAavg), colour="blue", size=1) +
   geom_point(aes(y=Tower_TScor), colour="red", size=1)+
   annotate("text", label=paste("r=", as.character(round(eval(cor(temp$Tower_TAavg, temp$Tower_TScor, use="complete.obs")),digits=3))), x=as.Date("2010-10-05"), y=40, fontface="bold")+
   labs(title="Time Series MMF", y="Temperature (c)",x="Date") +theme_minimal()
-  
+
+temp$year <- substr(as.character(temp$date),1,4)
+yr_2010 <- subset(temp, year==2010)
+
+ggplot(yr_2010, aes(x=date)) + 
+  geom_line(aes(y=Tower_TAavg), colour="blue", size=1) +
+  geom_line(aes(y=Tower_TScor), colour="red", size=1)+
+  annotate("text", label=paste("r=", as.character(round(eval(cor(yr_2010$Tower_TAavg, yr_2010$Tower_TScor, use="complete.obs")),digits=3))), x=as.Date("2010-10-05"), y=40, fontface="bold")+
+  labs(title="Time Series MMF", y="Temperature (c)",x="Date") +theme_minimal()
+
+ggplot(yr_2010, aes(x=date)) + 
+  geom_line(aes(y=Tower_TAmin), colour="blue", size=1) +
+  geom_line(aes(y=Tower_TSmin), colour="red", size=1)+
+  annotate("text", label=paste("r=", as.character(round(eval(cor(yr_2010$Tower_TAmin, yr_2010$Tower_TSmin, use="complete.obs")),digits=3))), x=as.Date("2010-10-05"), y=40, fontface="bold")+
+  labs(title="Time Series MMF", y="Temperature (c)",x="Date") +theme_minimal()
+
+ggplot(yr_2010, aes(x=date)) + 
+  geom_line(aes(y=Tower_TAmax), colour="blue", size=1) +
+  geom_line(aes(y=Tower_TSmax), colour="red", size=1)+
+  annotate("text", label=paste("r=", as.character(round(eval(cor(yr_2010$Tower_TAmax, yr_2010$Tower_TSmax, use="complete.obs")),digits=3))), x=as.Date("2010-10-05"), y=40, fontface="bold")+
+  labs(title="Time Series MMF", y="Temperature (c)",x="Date") +theme_minimal()
+
+
 #2) Pull in anciallary datasets: Daymet, Modis LST, and Gridmet. Gridmet will be trickiest. 
 #2a) Daymet
 Dk1Daymet <- read.csv("C:/Users/malbarn/Documents/LST_Project/Initial_Met_Comparisons/11211_lat_35.9712_lon_-79.09338_2018-09-17_133048.csv", skip=7)
@@ -94,9 +115,27 @@ Dk2Daymet$Daymet_Tavg <- rowMeans(Dk2Daymet[c('Daymet_Tmax', 'Daymet_Tmin')], na
 MmsDaymet <- read.csv("C:/Users/malbarn/Documents/LST_Project/Initial_Met_Comparisons/11567_lat_39.3232_lon_-86.4131_2018-09-17_132853.csv", skip=7)
 MmsDaymet$date <- as.Date(paste(MmsDaymet$year, MmsDaymet$yday, sep="-"), format="%Y-%j")
 head(MmsDaymet)
+tail(MmsDaymet)
 MmsDaymet <- plyr::rename(MmsDaymet, replace=c("tmax..deg.c." = "Daymet_Tmax", "tmin..deg.c."="Daymet_Tmin"))
 MmsDaymet <- MmsDaymet[,c("date","Daymet_Tmax","Daymet_Tmin")]
 MmsDaymet$Daymet_Tavg <- rowMeans(MmsDaymet[c('Daymet_Tmax', 'Daymet_Tmin')], na.rm=TRUE)
+
+Mms_daymet_flux <- merge(temp,MmsDaymet)
+cor(Mms_daymet_flux$Tower_TScor, Mms_daymet_flux$Daymet_Tavg)
+cor(Mms_daymet_flux$Tower_TAavg, Mms_daymet_flux$Daymet_Tavg)
+ggplot(Mms_daymet_flux, aes(x=date)) + 
+  geom_point(aes(y=Tower_TScor), colour="blue", size=1) +
+  geom_point(aes(y=Tower_TAavg), colour="red", size=1)+
+  geom_point(aes(y=Daymet_Tavg), colour="green", size=1)+
+  annotate("text", label=paste("r=", as.character(round(eval(cor(Mms_daymet_flux$Tower_TAavg, Mms_daymet_flux$Daymet_Tavg, use="complete.obs")),digits=3))), x=as.Date("2010-10-05"), y=40, fontface="bold")+
+  labs(title="Time Series MMF", y="Temperature (c)",x="Date") +theme_minimal()
+
+cor(Mms_daymet_flux$Tower_TSmax, Mms_daymet_flux$Daymet_Tmax)
+cor(Mms_daymet_flux$Tower_TAmax, Mms_daymet_flux$Daymet_Tmax)
+
+cor(Mms_daymet_flux$Tower_TSmin, Mms_daymet_flux$Daymet_Tmin)
+cor(Mms_daymet_flux$Tower_TAmin, Mms_daymet_flux$Daymet_Tmin)
+
 
 #2b) MODIS LST
 MODISDk1 <- read.csv("C:/Users/malbarn/Documents/LST_Project/MODIS_LST/MODIS_LST_dk1.csv")
@@ -117,7 +156,18 @@ MODISMms <- MODISMms[c("date", "Modis_Temp")]
 Gridmet_Tmax_mms <- read.csv("C:/Users/malbarn/Documents/LST_Project/Initial_Met_Comparisons/tmax_mms_gridmet.csv")
 head(Gridmet_Tmax_mms)
 Gridmet_Tmax_mms$date <- as.Date(Gridmet_Tmax_mms$system.time_start, format="%b %d, %Y")
-Gridmet_Tmax_mms$gridmettmax <-as.numeric(Gridmet_Tmax_mms$tmmx) - 273.15
+Gridmet_Tmax_mms$Gridmet_Tmax <-as.numeric(Gridmet_Tmax_mms$tmmx) - 273.15
+
+tmingridmet_mms<- read.csv("C:/Users/malbarn/Documents/LST_Project/Initial_Met_Comparisons/tmin_mms_gridmet.csv")
+head(tmingridmet_mms)
+tmingridmet_mms$date <- as.Date(tmingridmet_mms$system.time_start, format="%b %d, %Y")
+tmingridmet_mms$Gridmet_Tmin <-as.numeric(tmingridmet_mms$tmmn) - 273.15
+
+
+Gridmetmms <- merge(tmingridmet_mms, Gridmet_Tmax_mms, by="date")
+Gridmetmms <- subset(Gridmetmms, select=-c(system.time_start.x, system.time_start.y, tmmn, tmmx))
+
+
 Gridmet_Tmax_dk1 <- read.csv("C:/Users/malbarn/Documents/LST_Project/Initial_Met_Comparisons/tmax_dk1_gridmet.csv")
 Gridmet_Tmax_dk1$date <- as.Date(Gridmet_Tmax_dk1$system.time_start, format="%b %d, %Y")
 Gridmet_Tmax_dk1$Gridmet_Tmax <-as.numeric(Gridmet_Tmax_dk1$tmmx) - 273.15
@@ -125,18 +175,15 @@ Gridmet_Tmax_dk2 <- read.csv("C:/Users/malbarn/Documents/LST_Project/Initial_Met
 Gridmet_Tmax_dk2$date <- as.Date(Gridmet_Tmax_dk2$system.time_start, format="%b %d, %Y")
 Gridmet_Tmax_dk2$Gridmet_Tmax <-as.numeric(Gridmet_Tmax_dk2$tmmx) - 273.15
 
-tmingridmet_mms<- read.csv("C:/Users/malbarn/Documents/LST_Project/Initial_Met_Comparisons/tmin_mms_gridmet.csv")
-head(tmingridmet_mms)
-tmingridmet_mms$date <- as.Date(tmingridmet_mms$system.time_start, format="%b %d, %Y")
-tmingridmet_mms$Gridmet_Tmin <-as.numeric(tmingridmet_mms$tmmn) - 273.15
+
 tmingridmet_dk1 <- read.csv("C:/Users/malbarn/Documents/LST_Project/Initial_Met_Comparisons/tmin_dk1_gridmet.csv")
 tmingridmet_dk1$date <- as.Date(tmingridmet_dk1$system.time_start, format="%b %d, %Y")
 tmingridmet_dk1$Gridmet_Tmin <-as.numeric(tmingridmet_dk1$tmmn) - 273.15
+
 tmingridmet_dk2 <- read.csv("C:/Users/malbarn/Documents/LST_Project/Initial_Met_Comparisons/tmin_dk2_gridmet.csv")
 tmingridmet_dk2$date <- as.Date(tmingridmet_dk2$system.time_start, format="%b %d, %Y")
 tmingridmet_dk2$Gridmet_Tmin <-as.numeric(tmingridmet_dk2$tmmn) - 273.15
 
-Gridmetmms <- merge(tmingridmet_mms, Gridmet_Tmax_mms, by="date")
 Gridmetdk1 <- merge(tmingridmet_dk1, Gridmet_Tmax_dk1)
 Gridmetdk2 <- merge(tmingridmet_dk2, Gridmet_Tmax_dk2)
 
