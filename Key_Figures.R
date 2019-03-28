65948fa42eea9339ed4f8bdd04c316032618d54d
malloryb <malbarn@indiana.edu>
2019-03-28 15:16:19 -0400

    Calculated Ta-Ts for the study region. Plotting now

--- Key_Figures.R
+++ Key_Figures.R
@@ -18,6 +18,7 @@ library(spatialEco)
 library(greenbrown)
 library(RColorBrewer)
 library(MODIS)
+library(rasterVis)
 fname <- file.choose("air.mon.mean.v501.nc")
 nc<-nc_open(fname)
 # Get a list of the NetCDF's R attributes:
@@ -102,7 +103,7 @@ LST <- stack(lapply(flist, Process_LST))
 LST_Jan <- stack(LST[[1]], LST[[2]], LST[[3]], LST[[4]])
 Jan_mean <- calc(LST_Jan, mean_na)
 print("Jan_mean")
-LST_Feb <- stack(LST[[5]], LST[[6]], LST[[7]], LST[[8]])
+LST_Feb <- stack(LST[[5]], LST[[6]], LST[[8]], LST[[8]])
 Feb_mean <- calc(LST_Feb, mean_na)
 print("Feb_mean")
 LST_Mar <- stack(LST[[9]], LST[[10]], LST[[11]], LST[[12]])
@@ -137,42 +138,203 @@ Dec_mean <- calc(LST_Dec, mean_na)
 print("Dec_mean")
 yearstack <- stack(Jan_mean, Feb_mean, Mar_mean, Apr_mean, May_mean, Jun_mean, Jul_mean, Aug_mean, Sep_mean, Oct_mean, Nov_mean, Dec_mean)
 return(yearstack)
-
 }
 
 LST_2001 <- Monthly_LST(2001)
 writeRaster(LST_2001, "/Users/mallory/Documents/Temp_Project/MODIS_LST_2001.tif")
+#plot(brick("/Users/mallory/Documents/Temp_Project/MODIS_LST_2001.tif"))
 
 LST_2002 <- Monthly_LST(2002)
-writeRaster(LST_2002, "MODIS_LST_2002.tif")
+writeRaster(LST_2002, "/Users/mallory/Documents/Temp_Project/MODIS_LST_2002.tif")
 
 LST_2003 <- Monthly_LST(2003)
-writeRaster(LST_2003, "MODIS_LST_2003.tif")
+writeRaster(LST_2003, "/Users/mallory/Documents/Temp_Project/MODIS_LST_2003.tif")
 
 LST_2004 <- Monthly_LST(2004)
-writeRaster(LST_2004, "MODIS_LST_2004.tif")
-
+writeRaster(LST_2004, "/Users/mallory/Documents/Temp_Project/MODIS_LST_2004.tif")
 
 LST_2005 <- Monthly_LST(2005)
-writeRaster(LST_2005, "MODIS_LST_2005.tif")
+writeRaster(LST_2005, "/Users/mallory/Documents/Temp_Project/MODIS_LST_2005.tif")
 
 LST_2006 <- Monthly_LST(2006)
-writeRaster(LST_2006, "MODIS_LST_2006.tif")
+writeRaster(LST_2006, "/Users/mallory/Documents/Temp_Project/MODIS_LST_2006.tif")
 
 LST_2007 <- Monthly_LST(2007)
-writeRaster(LST_2007, "MODIS_LST_2007.tif")
+writeRaster(LST_2007, "/Users/mallory/Documents/Temp_Project/MODIS_LST_2007.tif")
 
 LST_2008 <- Monthly_LST(2008)
-writeRaster(LST_2008, "MODIS_LST_2008.tif")
+writeRaster(LST_2008, "/Users/mallory/Documents/Temp_Project/MODIS_LST_2008.tif")
 
 LST_2009 <- Monthly_LST(2009)
-writeRaster(LST_2009, "MODIS_LST_2009.tif")
+writeRaster(LST_2009, "/Users/mallory/Documents/Temp_Project/MODIS_LST_2009.tif")
 
 LST_2010 <- Monthly_LST(2010)
-writeRaster(LST_2010, "MODIS_LST_2010.tif")
+writeRaster(LST_2010, "/Users/mallory/Documents/Temp_Project/MODIS_LST_2010.tif")
 
 LST_2011 <- Monthly_LST(2011)
-writeRaster(LST_2011, "MODIS_LST_2011.tif")
+writeRaster(LST_2011, "/Users/mallory/Documents/Temp_Project/MODIS_LST_2011.tif")
 
 LST_2012 <- Monthly_LST(2012)
-writeRaster(LST_2012, "MODIS_LST_2012.tif")
+writeRaster(LST_2012, "/Users/mallory/Documents/Temp_Project/MODIS_LST_2012.tif")
+
+LST_2013 <- Monthly_LST(2013)
+writeRaster(LST_2013, "/Users/mallory/Documents/Temp_Project/MODIS_LST_2013.tif")
+
+LST_2014 <- Monthly_LST(2014)
+writeRaster(LST_2014, "/Users/mallory/Documents/Temp_Project/MODIS_LST_2014.tif")
+
+LST_2015 <- Monthly_LST(2015)
+writeRaster(LST_2015, "/Users/mallory/Documents/Temp_Project/MODIS_LST_2015.tif")
+
+LST_2016 <- Monthly_LST(2016)
+writeRaster(LST_2016, "/Users/mallory/Documents/Temp_Project/MODIS_LST_2016.tif")
+
+LST_2017 <- Monthly_LST(2017)
+writeRaster(LST_2017, "/Users/mallory/Documents/Temp_Project/MODIS_LST_2017.tif")
+
+#Join up LST and daymet: crop daymet and reproject to proper extent
+#Create function per year that returns: 3) Ta-Ts (for all 12 months)
+#Increase raster chunk size for this 
+rasterOptions(tmpdir="C:\\",tmptime = 24,progress="text",timer=TRUE,overwrite = T,chunksize=2e+08,maxmemory=1e+8)
+
+
+Monthly_Ta_Ts <- function(year){
+  filenamelst <- paste0("/Users/mallory/Documents/Temp_Project/MODIS_LST_", year, ".tif", sep="")
+  filenamedaymet1 <- paste0("/Users/mallory/Documents/Temp_Project/Daymet/daymet_v3_tmax_monavg_", year, "_na.tif")
+  #filenamedaymet2 <- paste0("/Users/mallory/Documents/Temp_Project/Daymet/daymet_v3_tmin_monavg_", year, "_na.tif")
+  LST <- stack(filenamelst)
+  daymet <- stack(filenamedaymet1)
+  #Tmin <- stack(filenamedaymet2)
+  #Ta <- overlay(Tmax, Tmin, fun=mean_na)
+  #Crop
+  e2 <- extent(-40000, 2300000, -1600000, 400000)
+  print("initial crop")
+  cropped <- crop(daymet, e2)
+  #cropped2 <- calc(cropped, fun = mean)
+  print("projecting raster")
+  #Project raster to lat/long coordinates
+  projected_raster <- projectRaster(cropped, crs="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+  ext <- extent(LST)
+  Ta_res <- crop(projected_raster, ext)
+  print("resampling")
+  Ta<- resample(Ta_res,LST,method='bilinear')
+  #Crop to study extent
+  stack(Ta, LST )
+  plot(LST)
+  plot(Ta)
+  print("Subtracting")
+  Ta_Ts <- Ta-LST
+  plot(Ta_Ts)
+  return(Ta_Ts)
+}
+
+Diff_2001 <- Monthly_Ta_Ts(2001)
+Diff_2002 <- Monthly_Ta_Ts(2002)
+Diff_2003 <- Monthly_Ta_Ts(2003)
+Diff_2004 <- Monthly_Ta_Ts(2004)
+Diff_2005 <- Monthly_Ta_Ts(2005)
+Diff_2006 <- Monthly_Ta_Ts(2006)
+Diff_2007 <- Monthly_Ta_Ts(2007)
+Diff_2008 <- Monthly_Ta_Ts(2008)
+Diff_2009 <- Monthly_Ta_Ts(2009)
+Diff_2010 <- Monthly_Ta_Ts(2010)
+Diff_2011 <- Monthly_Ta_Ts(2011)
+Diff_2012 <- Monthly_Ta_Ts(2012)
+Diff_2013 <- Monthly_Ta_Ts(2013)
+Diff_2014 <- Monthly_Ta_Ts(2014)
+Diff_2015 <- Monthly_Ta_Ts(2015)
+Diff_2016 <- Monthly_Ta_Ts(2016)
+Diff_2017 <- Monthly_Ta_Ts(2017)
+
+Jan_Diffs <- stack(Diff_2001[[1]], Diff_2002[[1]], Diff_2003[[1]], Diff_2004[[1]],
+                   Diff_2005[[1]], Diff_2006[[1]], Diff_2007[[1]], Diff_2008[[1]],
+                   Diff_2009[[1]], Diff_2010[[1]], Diff_2011[[1]], Diff_2012[[1]],
+                   Diff_2013[[1]], Diff_2014[[1]], Diff_2015[[1]], Diff_2016[[1]],
+                   Diff_2017[[1]])
+Jan_Diffs_Mean <- calc(Jan_Diffs, mean_na)
+plot(Jan_Diffs_Mean)
+Feb_Diffs <- stack(Diff_2001[[2]], Diff_2002[[2]], Diff_2003[[2]], Diff_2004[[2]],
+                   Diff_2005[[2]], Diff_2006[[2]], Diff_2007[[2]], Diff_2008[[2]],
+                   Diff_2009[[2]], Diff_2010[[2]], Diff_2011[[2]], Diff_2012[[2]],
+                   Diff_2013[[2]], Diff_2014[[2]], Diff_2015[[2]], Diff_2016[[2]],
+                   Diff_2017[[2]])
+Feb_Diffs_Mean <- calc(Feb_Diffs, mean_na)
+Mar_Diffs <- stack(Diff_2001[[3]], Diff_2002[[3]], Diff_2003[[3]], Diff_2004[[3]],
+                   Diff_2005[[3]], Diff_2006[[3]], Diff_2007[[3]], Diff_2008[[3]],
+                   Diff_2009[[3]], Diff_2010[[3]], Diff_2011[[3]], Diff_2012[[3]],
+                   Diff_2013[[3]], Diff_2014[[3]], Diff_2015[[3]], Diff_2016[[3]],
+                   Diff_2017[[3]])
+Mar_Diffs_Mean <- calc(Mar_Diffs, mean_na)
+Apr_Diffs <- stack(Diff_2001[[4]], Diff_2002[[4]], Diff_2003[[4]], Diff_2004[[4]],
+                   Diff_2005[[4]], Diff_2006[[4]], Diff_2007[[4]], Diff_2008[[4]],
+                   Diff_2009[[4]], Diff_2010[[4]], Diff_2011[[4]], Diff_2012[[4]],
+                   Diff_2013[[4]], Diff_2014[[4]], Diff_2015[[4]], Diff_2016[[4]],
+                   Diff_2017[[4]])
+Apr_Diffs_Mean <- calc(Apr_Diffs, mean_na)
+May_Diffs <- stack(Diff_2001[[5]], Diff_2002[[5]], Diff_2003[[5]], Diff_2004[[5]],
+                   Diff_2005[[5]], Diff_2006[[5]], Diff_2007[[5]], Diff_2008[[5]],
+                   Diff_2009[[5]], Diff_2010[[5]], Diff_2011[[5]], Diff_2012[[5]],
+                   Diff_2013[[5]], Diff_2014[[5]], Diff_2015[[5]], Diff_2016[[5]],
+                   Diff_2017[[5]])
+May_Diffs_Mean <- calc(May_Diffs, mean_na)
+Jun_Diffs <- stack(Diff_2001[[6]], Diff_2002[[6]], Diff_2003[[6]], Diff_2004[[6]],
+                   Diff_2005[[6]], Diff_2006[[6]], Diff_2007[[6]], Diff_2008[[6]],
+                   Diff_2009[[6]], Diff_2010[[6]], Diff_2011[[6]], Diff_2012[[6]],
+                   Diff_2013[[6]], Diff_2014[[6]], Diff_2015[[6]], Diff_2016[[6]],
+                   Diff_2017[[6]])
+Jun_Diffs_Mean <- calc(Jun_Diffs, mean_na)
+
+Jul_Diffs <- stack(Diff_2001[[7]], Diff_2002[[7]], Diff_2003[[7]], Diff_2004[[7]],
+                   Diff_2005[[7]], Diff_2006[[7]], Diff_2007[[7]], Diff_2008[[7]],
+                   Diff_2009[[7]], Diff_2010[[7]], Diff_2011[[7]], Diff_2012[[7]],
+                   Diff_2013[[7]], Diff_2014[[7]], Diff_2015[[7]], Diff_2016[[7]],
+                   Diff_2017[[7]])
+Jul_Diffs_Mean <- calc(Jul_Diffs, mean_na)
+
+Aug_Diffs <- stack(Diff_2001[[8]], Diff_2002[[8]], Diff_2003[[8]], Diff_2004[[8]],
+                   Diff_2005[[8]], Diff_2006[[8]], Diff_2007[[8]], Diff_2008[[8]],
+                   Diff_2009[[8]], Diff_2010[[8]], Diff_2011[[8]], Diff_2012[[8]],
+                   Diff_2013[[8]], Diff_2014[[8]], Diff_2015[[8]], Diff_2016[[8]],
+                   Diff_2017[[8]])
+Aug_Diffs_Mean <- calc(Aug_Diffs, mean_na)
+Sep_Diffs <- stack(Diff_2001[[9]], Diff_2002[[9]], Diff_2003[[9]], Diff_2004[[9]],
+                   Diff_2005[[9]], Diff_2006[[9]], Diff_2007[[9]], Diff_2008[[9]],
+                   Diff_2009[[9]], Diff_2010[[9]], Diff_2011[[9]], Diff_2012[[9]],
+                   Diff_2013[[9]], Diff_2014[[9]], Diff_2015[[9]], Diff_2016[[9]],
+                   Diff_2017[[9]])
+Sep_Diffs_Mean <- calc(Sep_Diffs, mean_na)
+Oct_Diffs <- stack(Diff_2001[[10]], Diff_2002[[10]], Diff_2003[[10]], Diff_2004[[10]],
+                   Diff_2005[[10]], Diff_2006[[10]], Diff_2007[[10]], Diff_2008[[10]],
+                   Diff_2009[[10]], Diff_2010[[10]], Diff_2011[[10]], Diff_2012[[10]],
+                   Diff_2013[[10]], Diff_2014[[10]], Diff_2015[[10]], Diff_2016[[10]],
+                   Diff_2017[[10]])
+Oct_Diffs_Mean <- calc(Oct_Diffs, mean_na)
+Nov_Diffs <- stack(Diff_2001[[11]], Diff_2002[[11]], Diff_2003[[11]], Diff_2004[[11]],
+                   Diff_2005[[11]], Diff_2006[[11]], Diff_2007[[11]], Diff_2008[[11]],
+                   Diff_2009[[11]], Diff_2010[[11]], Diff_2011[[11]], Diff_2012[[11]],
+                   Diff_2013[[11]], Diff_2014[[11]], Diff_2015[[11]], Diff_2016[[11]],
+                   Diff_2017[[11]])
+Nov_Diffs_Mean <- calc(Nov_Diffs, mean_na)
+Dec_Diffs <- stack(Diff_2001[[12]], Diff_2002[[12]], Diff_2003[[12]], Diff_2004[[12]],
+                   Diff_2005[[12]], Diff_2006[[12]], Diff_2007[[12]], Diff_2008[[12]],
+                   Diff_2009[[12]], Diff_2010[[12]], Diff_2011[[12]], Diff_2012[[12]],
+                   Diff_2013[[12]], Diff_2014[[12]], Diff_2015[[12]], Diff_2016[[12]],
+                   Diff_2017[[12]])
+Dec_Diffs_Mean <- calc(Dec_Diffs, mean_na)
+
+Diffs<- stack(Jan_Diffs_Mean, Feb_Diffs_Mean, Mar_Diffs_Mean, Apr_Diffs_Mean, May_Diffs_Mean,
+      Jun_Diffs_Mean, Jul_Diffs_Mean, Aug_Diffs_Mean, Sep_Diffs_Mean, Oct_Diffs_Mean, 
+      Nov_Diffs_Mean, Dec_Diffs_Mean)
+plot(Diffs)
+cuts=c(-6,-4,-2,0,2,4,6) #set breaks
+pal <- colorRampPalette(c("blue","lightblue", "white","red", "red3"))
+plot(Diffs, breaks=cuts, col = rev(pal(n=6)))
+
+cols <- colorRampPalette(brewer.pal(9,"RdBu"))
+# create a level plot - plot
+my.at <- seq(-6,6,1)
+levelplot(Diffs, at=my.at, main="Difference between Air Temperature and Surface Temperature (Ta-Ts)",
+          col.regions=rev(cols), pretty=TRUE)
+writeRaster(Diffs, "/Users/mallory/Documents/Temp_Project/Ta_Ts_All.tif")
+spatialEco::rasterCorrelation(Avg_Dif, Forest_Age, s = 3, type = "pearson", file.name = NULL)
+
