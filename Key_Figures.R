564bdf85d92db54edd3c0d9d3b3121ae1142aa76
malloryb <malbarn@indiana.edu>
2019-03-27 23:02:50 -0400

    Added print and write rasters

--- Key_Figures.R
+++ Key_Figures.R
@@ -73,6 +73,7 @@ setwd("/Users/mallory/Documents/APPEARS_LST/")
 mean_na <- function(x) {
   mean(x,na.rm=T)
 }
+
 Monthly_LST <- function(year){
 flist <- list.files(pattern= paste0("(LST_Day).*doy", year, sep=""))
 flist
@@ -95,37 +96,83 @@ Process_LST <- function(x){
     rast <- setNames(rast, date)
     return(rast)
 }
+
 LST <- stack(lapply(flist, Process_LST))
 
 LST_Jan <- stack(LST[[1]], LST[[2]], LST[[3]], LST[[4]])
 Jan_mean <- calc(LST_Jan, mean_na)
+print("Jan_mean")
 LST_Feb <- stack(LST[[5]], LST[[6]], LST[[7]], LST[[8]])
 Feb_mean <- calc(LST_Feb, mean_na)
+print("Feb_mean")
 LST_Mar <- stack(LST[[9]], LST[[10]], LST[[11]], LST[[12]])
 Mar_mean <- calc(LST_Mar, mean_na)
+print("Mar_mean")
 LST_Apr <- stack(LST[[13]], LST[[14]], LST[[15]])
 Apr_mean <- calc(LST_Apr, mean_na)
+print("Apr_mean")
 LST_May <- stack(LST[[16]], LST[[17]], LST[[18]], LST[[19]])
 May_mean <- calc(LST_May, mean_na)
+print("May_mean")
 LST_Jun <- stack(LST[[20]], LST[[21]], LST[[22]], LST[[23]])
 Jun_mean <- calc(LST_Jun, mean_na)
+print("Jun_mean")
 LST_Jul <- stack(LST[[24]], LST[[25]], LST[[26]], LST[[27]])
 Jul_mean <- calc(LST_Jul, mean_na)
+print("Jul_mean")
 LST_Aug <- stack(LST[[28]], LST[[29]], LST[[30]])
 Aug_mean <- calc(LST_Aug, mean_na)
+print("Aug_mean")
 LST_Sep <- stack(LST[[31]], LST[[32]], LST[[33]], LST[[34]])
 Sep_mean <- calc(LST_Sep, mean_na)
+print("Sep_mean")
 LST_Oct <- stack(LST[[35]], LST[[36]], LST[[37]])
 Oct_mean <- calc(LST_Oct, mean_na)
+print("Oct_mean")
 LST_Nov <- stack(LST[[38]], LST[[39]], LST[[40]], LST[[41]])
 Nov_mean <- calc(LST_Nov, mean_na)
+print("Nov_mean")
 LST_Dec <- stack(LST[[42]], LST[[43]], LST[[44]], LST[[45]])
 Dec_mean <- calc(LST_Dec, mean_na)
-
-yearstack <- stack(Jan_mean, Feb_mean, Mar_mean, Apr_mean, Mar_mean, Apr_mean, May_mean, Jun,_mean, Jul_mean, Aug_mean, Sep_mean, Oct_mean, Nov_mean, Dec_mean)
+print("Dec_mean")
+yearstack <- stack(Jan_mean, Feb_mean, Mar_mean, Apr_mean, May_mean, Jun_mean, Jul_mean, Aug_mean, Sep_mean, Oct_mean, Nov_mean, Dec_mean)
 return(yearstack)
+
 }
 
+LST_2001 <- Monthly_LST(2001)
+writeRaster(LST_2001, "/Users/mallory/Documents/Temp_Project/MODIS_LST_2001.tif")
+
+LST_2002 <- Monthly_LST(2002)
+writeRaster(LST_2002, "MODIS_LST_2002.tif")
+
+LST_2003 <- Monthly_LST(2003)
+writeRaster(LST_2003, "MODIS_LST_2003.tif")
+
+LST_2004 <- Monthly_LST(2004)
+writeRaster(LST_2004, "MODIS_LST_2004.tif")
 
 
+LST_2005 <- Monthly_LST(2005)
+writeRaster(LST_2005, "MODIS_LST_2005.tif")
 
+LST_2006 <- Monthly_LST(2006)
+writeRaster(LST_2006, "MODIS_LST_2006.tif")
+
+LST_2007 <- Monthly_LST(2007)
+writeRaster(LST_2007, "MODIS_LST_2007.tif")
+
+LST_2008 <- Monthly_LST(2008)
+writeRaster(LST_2008, "MODIS_LST_2008.tif")
+
+LST_2009 <- Monthly_LST(2009)
+writeRaster(LST_2009, "MODIS_LST_2009.tif")
+
+LST_2010 <- Monthly_LST(2010)
+writeRaster(LST_2010, "MODIS_LST_2010.tif")
+
+LST_2011 <- Monthly_LST(2011)
+writeRaster(LST_2011, "MODIS_LST_2011.tif")
+
+LST_2012 <- Monthly_LST(2012)
+writeRaster(LST_2012, "MODIS_LST_2012.tif")
